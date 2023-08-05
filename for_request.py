99c99468994669bfeadea26d5d22f40b52b8bcc7
StipRabin <117095282+StipRabyn@users.noreply.github.com>
2023-08-05 16:03:47 +0300

    помргите

---
+++ for_request.py
@@ -0,0 +1,28 @@
+user_agent_list = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/115.0.5790.160 Mobile/15E148 Safari/604.1'
+]
+
+
+referer_list = [
+    'https://duckduckgo.com/',
+    'https://www.google.com/',
+    'https://yandex.com/',
+    'https://www.yahoo.com/',
+    'https://mail.ru/',
+    'https://www.bing.com/',
+    'https://nigma.net.ru/',
+    'https://www.rambler.ru/',
+    'https://vk.com/'
+]
+
+
+data_list = [
+    ['tv1.lordfilm.lu', 'https://tv1.lordfilm.lu'],
+    ['tv1.lordfilm.black', 'https://tv1.lordfilm.black'],
+    ['Jelang', 'http://jelang.ru']
+]
