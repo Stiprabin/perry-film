b201a827ac9d4b209143821867404f863c444984
StipRabin <117095282+StipRabyn@users.noreply.github.com>
2023-08-05 16:02:48 +0300

    помргите

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import (
+    Flask,
+    request,
+    jsonify,
+    Response
+)
+from parsers import parser_lordfilm, parser_jelang
+from for_request import data_list
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home() -> str:
+    return "Welcome to the club, buddy!"
+
+
+@app.route('/', methods=['POST'])
+def post() -> Response:
+    url = request.form['url']
+    query = request.form['query']
+
+    print(f"Запрос \"{query}\" получен!")
+
+    if url == data_list[0][0]:
+        films: list = parser_lordfilm.parser(
+            url=data_list[0][1],
+            query=query,
+            search_id="ajax_search"
+        )
+    elif url == data_list[1][0]:
+        films: list = parser_lordfilm.parser(
+            url=data_list[1][1],
+            query=query,
+            search_id="story"
+        )
+    else:
+        films: list = parser_jelang.parser(
+            url=data_list[-1][1],
+            query=query
+        )
+
+    return jsonify(films)
