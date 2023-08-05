480f2b0d7661d0eb48b79aef814cc7e68437a434
StipRabin <117095282+StipRabyn@users.noreply.github.com>
2023-08-05 16:06:59 +0300

    помргите

---
+++ unchromedriver_manager.py
@@ -0,0 +1,29 @@
+import random
+import undetected_chromedriver as uc
+
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from for_request import user_agent_list
+
+
+# контекстный менеджер
+class UnChromedriverManager:
+
+    def __init__(self) -> None:
+        # опции браузера
+        self.chrome_options = ChromeOptions()
+        self.chrome_options.page_load_strategy = "eager"
+        self.chrome_options.add_argument("--headless")
+        self.chrome_options.add_argument("--no-sandbox")
+        self.chrome_options.add_argument("--start-maximized")
+        self.chrome_options.add_argument("--disable-dev-shm-usage")
+        self.chrome_options.add_argument("--disable-infobars")
+        self.chrome_options.add_argument("--disable-extensions")
+        self.chrome_options.add_argument("--user-agent=" + random.choice(user_agent_list))
+
+    def __enter__(self) -> uc.Chrome:
+        self.driver = uc.Chrome(options=self.chrome_options)
+        return self.driver
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.driver.close()
+        self.driver.quit()
