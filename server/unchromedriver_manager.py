e6e428e0c2caea1de3b783298dc1d8235f9915eb
StipRabin <117095282+StipRabyn@users.noreply.github.com>
2023-08-05 23:48:49 +0300

    помргите

--- server/unchromedriver_manager.py
+++ server/unchromedriver_manager.py
@@ -26,8 +26,7 @@ class UnChromedriverManager:
     def __enter__(self) -> uc.Chrome:
         self.driver = uc.Chrome(
             version_main=114,
-            executable_path=os.environ.get("CHROMEDRIVER_PATH"),
-            browser_executable_path="/snap/bin/chromium",
+            browser_executable_path=os.environ.get("CHROMEDRIVER_PATH"),
             options=self.chrome_options
         )
         return self.driver
