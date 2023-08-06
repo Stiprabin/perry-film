32e8392b95fbb6bbcef87daea333e222bc59b556
StipRabin <117095282+StipRabyn@users.noreply.github.com>
2023-08-06 12:41:53 +0300

    помргите

--- server/unchromedriver_manager.py
+++ server/unchromedriver_manager.py
@@ -1,4 +1,3 @@
-import os
 import random
 import undetected_chromedriver as uc
 
@@ -11,23 +10,22 @@ class UnChromedriverManager:
     def __init__(self) -> None:
         # опции браузера
         self.chrome_options = uc.ChromeOptions()
-
-        self.chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         self.chrome_options.page_load_strategy = "eager"
-
-        self.chrome_options.add_argument("--headless")
-        self.chrome_options.add_argument("--no-sandbox")
-        self.chrome_options.add_argument("--start-maximized")
-        self.chrome_options.add_argument("--disable-dev-shm-usage")
-        self.chrome_options.add_argument("--disable-infobars")
-        self.chrome_options.add_argument("--disable-extensions")
-        self.chrome_options.add_argument("--user-agent=" + random.choice(user_agent_list))
+        self.chrome_options.arguments.extend([
+            '--headless',
+            '--start-maximized',
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-infobars',
+            '--disable-extensions',
+            '--user-agent=' + random.choice(user_agent_list)
+        ])
 
 
     def __enter__(self) -> uc.Chrome:
         self.driver = uc.Chrome(
             version_main=114,
-            executable_path=os.environ.get("CHROMEDRIVER_PATH"),
             options=self.chrome_options
         )
         return self.driver
